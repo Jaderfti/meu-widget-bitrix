7cddc671797b9c752fad073fbc63e220695d274a
Jaderfti <jaderfti@gmail.com>
2025-05-30 16:10:42 -0300

    Add files via upload

---
+++ install.php
@@ -0,0 +1,9 @@
+<?php
+// Este arquivo é como a "capa" do nosso widget
+$arAppDescription = array(
+    'NAME' => 'Salvar Dados do Lead',  // Nome do nosso botão mágico
+    'DESCRIPTION' => 'Salva nome e telefone do lead',
+    'VERSION' => '1.0',
+    'VENDOR' => 'Seu Nome'
+);
+?>
